170c2329c524263c90fb086f62156db52e7555a5
azaza <az.dawletbaew@uandex.ru>
2022-10-16 23:59:41 +0500

    add func FillArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string[] FillArray(){
+int r = new  Random().Next(5, 10);
+Console.WriteLine(r);
+string[] array = new string[r];
+for (int i= 0; i<r; i++){
+     int randomLenght = new  Random().Next(8);
+     for (int j = 0 ; j<=randomLenght; j++ ){
+          array[i] += "w";
+     }
+}
+Console.WriteLine(String.Join(", ", array));
+return array;}
+FillArray();
